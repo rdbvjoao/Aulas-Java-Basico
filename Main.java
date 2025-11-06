c350b2d2c994efac3c098b7229221c4223d7df1c
João Vitor Borges dos Reis <joaovitordosreis2002@gmail.com>
2025-11-06 07:42:33 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		double [][] notasAlunos = new double[30][4] //30 linhas e 4 colunas
+		
+		//Primeiro aluno
+		notasAlunos[0][0] = 10;
+		notasAlunos[0][1] = 7;
+		notasAlunos[0][2] = 9;
+		notasAlunos[0][3] = 9.5;
+		
+		//Segundo aluno
+		notasAlunos[1][0] = 9;
+		notasAlunos[1][1] = 8;
+		notasAlunos[1][2] = 7;
+	    notasAlunos[1][3] = 9;
+	    
+	    //Terceiro aluno
+		notasAlunos[1][0] = 9;
+		notasAlunos[1][1] = 8;
+		notasAlunos[1][2] = 7;
+	    notasAlunos[1][3] = 9;
+	    
+	}
+}
